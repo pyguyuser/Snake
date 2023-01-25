bc4696d9f9ba44c9cd14756824e5f239e34ba904
pyguyuser <knanton39@gmail.com>
2023-01-25 21:28:58 +0300

    v0.1

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import pygame
 
-print(1)
+class Snake:
+    def __init__(self,color,x,y):
+        self.COLOR = color
+        self.X = x
+        self.Y = y
+        self.display = pygame.display.set_mode((self.X,self.Y))
 
-print('хэллоу ворлд')
-print('хэллоу ворлд')
+    def update(self):
+        pass
+
+snake = Snake((0,0,255),700,700)
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    snake.update()
+
+    pygame.display.update()
+
+pygame.quit()
